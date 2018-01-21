e7bb0e0a31edcae9c7a8d5f0cdb82cec7cede982
Cloud User <ec2-user@ip-172-31-30-53.us-east-2.compute.internal>
2018-01-21 12:23:09 +0000

    first version of workstation

---
+++ recipes/setup.rb
@@ -0,0 +1,15 @@
+package 'tree' do 
+action :install
+end
+package 'ntp' do
+action :install
+end
+package 'git' do
+action :install
+end
+file '/etc/motd' do 
+content 'This server is the property of narendar...'
+action :create
+owner 'root'
+group 'root'
+end
